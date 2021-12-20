8ebe5c28a136375086efc96f78e26876889d43e7
pbean720 <79056706+pbean720@users.noreply.github.com>
2021-12-19 18:58:32 -0600

    Update app.py

--- app.py
+++ app.py
@@ -4,9 +4,10 @@ window = tk.Tk() # <-- the Tk() class causes a window to open; we've assigned th
 window.title("Python Tkinter Calculator")
 
 window.configure(background="black") # <-- makes the background of the window black; you can also use hexadecimals
+window.geometry("600x600")
 
 canvas = tk.Canvas(window, background="purple", width=400, height=500) # <-- canvas is our window frame/ the inside part of our window
-canvas.grid(columnspan=4, rowspan=5) # we create a number of grid rows and columns that are invisible
+canvas.grid(columnspan=4, rowspan=6) # we create a number of grid rows and columns that are invisible
 
 """
         C O L U M N S
@@ -15,9 +16,11 @@ R   1.0  1.1  1.2  1.3
 O   2.0  2.1  2.2  2.3   
 W   3.0  3.1  3.2  3.3  
 S   4.0  4.1  4.2  4.3   
+    5.0  5.1  5.2  5.3 
 """
+
 # INPUT BOX
-entry = tk.Entry(window) .grid()
+entry = tk.Entry(window) .grid(row=0, column=0, columnspan=3)
 
 
 # CALCULATOR BUTTONS
@@ -25,7 +28,7 @@ entry = tk.Entry(window) .grid()
 BtnsArr = ["clrBtn", "sqrBtn", "sqrRtBtn", "plusBtn", "SvnBtn", "EigBtn", "NineBtn", "minusBtn", "FourBtn", "FiveBtn", "SixBtn", "mulBtn", "oneBtn", "TwoBtn", "ThreeBtn", "divBtn", "decBtn", "zeroBtn", "expBtn", "equalBtn"]
 BtnsTextArr = ["C", "sqr()", "√", "+", "7", "8", "9", "-", "4", "5", "6", "x", "1", "2", "3", "/", ".", "0", "exp", "="]
 
-r = 0
+r = 1
 num1 = 0
 num2 = 0
 while num1 <= 4:
